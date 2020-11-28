44038455e6d86add4e60f4179aa33f92c7ac1c6d
Jonathan Sou <jonathansou7@gmail.com>
2020-11-27 22:38:44 -0800

    Added furnished click functionality

--- src/components/FilterWidget/index.tsx
+++ src/components/FilterWidget/index.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Routes } from '../../utils/routes';
+import { FilterContext } from '../../provider/FilterContext';
 
 import CityFilter from './CityFilter';
 import DatePicker from './DatePicker';
 
 const FilterWidget = () => {
+  const { setIsFurnished } = useContext(FilterContext);
+
+  const onFurnishedClick = (furnished:boolean) => {
+    if (setIsFurnished) {
+      setIsFurnished(furnished);
+    }
+  }
+
   return (
     <React.Fragment>
       <div className='flex flex-col w-full border mb-8 rounded-lg border shadow'>
         <div className='flex w-full font-bold'>
-          <div className='uppercase flex-1 text-center bg-gray-700 text-white py-4 rounded-tl-lg'>Furnished</div>
-          <div className='uppercase flex-1 text-center bg-gray-100 py-4 rounded-tr-lg text-gray-500'>Unfurnished</div>
+          <div
+            className='uppercase flex-1 text-center bg-gray-700 text-white py-4 rounded-tl-lg cursor-pointer'
+            onClick={() => onFurnishedClick(true)}
+          >  
+            Furnished
+          </div>
+          <div
+            className='uppercase flex-1 text-center bg-gray-100 py-4 rounded-tr-lg text-gray-500 cursor-pointer'
+            onClick={() => onFurnishedClick(false)}
+          >
+            Unfurnished
+          </div>
         </div>
         <div>
           <CityFilter />
