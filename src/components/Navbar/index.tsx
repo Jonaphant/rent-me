1a56696665a93333dfd0b13d18b0b15a2ce61168
Jonathan Sou <jonathansou7@gmail.com>
2020-11-27 22:25:40 -0800

    Minor style changes

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,14 +4,14 @@ import { Routes } from '../../utils/routes';
 
 const Navbar = () => {
   return (
-    <nav className='w-full flex justify-between sticky top-0 z-20 bg-white shadow'>
-      <h1 className='font-mono text-5xl py-4 px-4 hover:opacity-75 cursor-pointer'>Rent Me</h1>
+    <nav className='w-full flex justify-between sticky top-0 z-20 bg-white shadow py-2'>
+      <h1 className='font-mono text-5xl py-4 px-4 hover:opacity-75 cursor-pointer'>RentMe</h1>
       <div className='flex items-center mx-4 font-bold text-gray-500'>
         <Link to={Routes.LandingPage} className='hover:opacity-75'>
           <span className='uppercase'>Find a rental</span>
         </Link>
         <Link to={Routes.LandingPage} className='mx-12 hover:opacity-75'>
-          <span className='uppercase'>Living in rentalme</span>
+          <span className='uppercase'>Living in rentme</span>
         </Link>
         <Link to={Routes.LandingPage} className='hover:opacity-75'>
           <button className='border-4 border-solid border-gray-500 rounded-full py-2 px-4 hover:opacity-75'>
