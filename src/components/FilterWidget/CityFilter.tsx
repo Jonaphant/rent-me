5f197c2ed53145a9285ee48e7b2af5f5f6f8c0b3
Jonathan Sou <jonathansou7@gmail.com>
2020-11-27 22:31:29 -0800

    Added city click functionality and brought in city state to manage chosen option

--- src/components/FilterWidget/CityFilter.tsx
+++ src/components/FilterWidget/CityFilter.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { FilterContext } from '../../provider/FilterContext';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown, faChevronUp, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
@@ -47,25 +49,34 @@ const cities: Array<CityProps> = [
 ]
 
 const CityFilter = () => {
+  const {city, setCity} = useContext(FilterContext);
   const [showDropDown, setShowDropDown] = useState<boolean>(false);
+
+  const onCityClick = (cityName: string) => {
+    if (setCity) {
+      setCity(cityName);
+    }
+    setShowDropDown(false);
+  }
 
   return (
     <React.Fragment>
-      <div className='bg-white py-6 flex justify-between px-6 border-b' onClick={() => setShowDropDown(prev => !prev)}>
+      <div className='bg-white p-6 flex items-center justify-between border-b cursor-pointer' onClick={() => setShowDropDown(prev => !prev)}>
         <div className='flex'>
           <FontAwesomeIcon icon={faMapMarkerAlt} className='text-yellow-500 fa-lg mr-8' />
-          <p className='font-semibold text-lg'>Choose a city</p>
+          <p className='font-semibold text-lg'>{city}</p>
         </div>
         <FontAwesomeIcon icon={showDropDown ? faChevronUp : faChevronDown} className='text-gray-200 fa-lg' />
       </div>
       <div className='z-20 bg-white'>
         {showDropDown && (
-          cities.map(city => (
+          cities.map(cityData => (
             <p
               className='pl-4 py-2 font-semibold cursor-pointer hover:text-yellow-500'
-              onClick={() => alert('Set chosen city to global state and close drop down')}
-              key={city.id}>
-              {city.name}
+              onClick={() => onCityClick(cityData.name)}
+              key={cityData.id}
+            >
+              {cityData.name}
             </p>
           ))
         )}
