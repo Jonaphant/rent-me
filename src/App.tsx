1bc0ae16b8045fb3f819efffadd4c4daeaf52329
Jonathan Sou <jonathansou7@gmail.com>
2020-11-27 22:30:06 -0800

    Wrapped provider around routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Routes } from './utils/routes';
 
+import { FilterProvider } from './provider/FilterContext';
+
 import Landing from './pages/Landing';
 import BrowseRentals from './pages/BrowseRentals';
 
 function App() {
   return (
     <Switch>
-      <Route path={Routes.LandingPage} component={Landing} exact />
-      <Route path={Routes.BrowseRentals} component={BrowseRentals} />
+      <FilterProvider>
+        <Route path={Routes.LandingPage} component={Landing} exact />
+        <Route path={Routes.BrowseRentals} component={BrowseRentals} />
+      </FilterProvider>
     </Switch>
   );
 }
