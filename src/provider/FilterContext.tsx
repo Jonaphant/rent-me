70a58a15d22d9b7a09b2b0b3b80f37bbd05a9e7a
Jonathan Sou <jonathansou7@gmail.com>
2020-11-28 20:13:45 -0800

    Created other filter states

--- src/provider/FilterContext.tsx
+++ src/provider/FilterContext.tsx
@@ -11,6 +11,10 @@ interface FilterContextProps {
   setFurnished?: Dispatch<SetStateAction<string>>,
   city: string,
   setCity?: Dispatch<SetStateAction<string>>,
+  neighborhood: string,
+  setNeighborhood?: Dispatch<SetStateAction<string>>,
+  bedrooms: string,
+  setBedrooms?: Dispatch<SetStateAction<string>>,
   rentalDate: RentalDateProps,
   setRentalDate?: Dispatch<SetStateAction<RentalDateProps>>,
 }
@@ -18,6 +22,8 @@ interface FilterContextProps {
 const defaultData = {
   furnished: 'Furnished',
   city: 'Choose a city',
+  neighborhood: 'Neighborhoods',
+  bedrooms: 'Bedrooms',
   rentalDate: {
     startDate: null,
     endDate: null,
@@ -29,13 +35,23 @@ export const FilterContext = createContext<FilterContextProps>(defaultData);
 export const FilterProvider = (props: { children: React.ReactNode; }) => {
   const [furnished, setFurnished] = useState('Furnished');
   const [city, setCity] = useState<string>('Choose a city');
+  const [neighborhood, setNeighborhood] = useState<string>('Neighborhoods');
+  const [bedrooms, setBedrooms] = useState<string>('Bedrooms');
   const [rentalDate, setRentalDate] = useState({
     startDate: null,
     endDate: null,
   });
 
   return (
-    <FilterContext.Provider value={{furnished, setFurnished, city, setCity, rentalDate, setRentalDate}}>
+    <FilterContext.Provider
+      value={{
+        furnished, setFurnished,
+        city, setCity,
+        rentalDate, setRentalDate,
+        neighborhood, setNeighborhood,
+        bedrooms, setBedrooms
+      }}
+    >
         {props.children}
     </FilterContext.Provider>
   )
