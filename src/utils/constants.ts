df8ce8f10c26398c75395872e4267de28a6826d0
Jonathan Sou <jonathansou7@gmail.com>
2020-11-28 16:30:41 -0800

    Moved dummy data to constants file

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,43 @@
-export enum Cities {
-  Atlanta = 'Atlanta',
-  Baltimore = 'Baltimore',
-  Boston = 'Boston',
-  Dallas = 'Dallas',
-  FortWorth = 'Forth Worth',
-  NewYork = 'New York',
-  Oakland = 'Oakland',
-  SanFrancisco = 'San Francisco',
-  SanJose = 'San Jose',
+interface CityProps {
+  value: string,
+  id: number,
 }
+
+export const CITIES: Array<CityProps> = [
+  {
+    value: 'Atlanta',
+    id: 0,
+  },
+  {
+    value: 'Baltimore',
+    id: 1,
+  },
+  {
+    value: 'Boston',
+    id: 2,
+  },
+  {
+    value: 'Dallas',
+    id: 3,
+  },
+  {
+    value: 'Forth Worth',
+    id: 4,
+  },
+  {
+    value: 'New York',
+    id: 5,
+  },
+  {
+    value: 'Oakland',
+    id: 6,
+  },
+  {
+    value: 'San Francisco',
+    id: 7,
+  },
+  {
+    value: 'San Jose',
+    id: 8,
+  }
+]
