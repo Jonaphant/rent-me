8eb241de9a28640e9d080f032ffdfec7e964acfe
Jonathan Sou <jonathansou7@gmail.com>
2020-11-28 17:13:39 -0800

    Added dummy data for other filter types

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
-interface CityProps {
+interface DummyDataProps {
   value: string,
   id: number,
 }
 
-export const CITIES: Array<CityProps> = [
+export const CITIES: Array<DummyDataProps> = [
   {
     value: 'Atlanta',
     id: 0,
@@ -41,3 +41,65 @@ export const CITIES: Array<CityProps> = [
     id: 8,
   }
 ]
+
+export const FURNISHED: Array<DummyDataProps> = [
+  {
+    value: 'Furnished',
+    id: 1,
+  },
+  {
+    value: 'Unfurnished',
+    id: 2,
+  },
+]
+
+export const NEIGHBORHOODS: Array<DummyDataProps> = [
+  {
+    value: 'Buckhead',
+    id: 1,
+  },
+  {
+    value: 'Brookhaven',
+    id: 2,
+  },
+  {
+    value: 'Vinings',
+    id: 3,
+  },
+  {
+    value: 'Decatur',
+    id: 4,
+  },
+  {
+    value: 'Midtown',
+    id: 5,
+  },
+  {
+    value: 'Downtown',
+    id: 6,
+  },
+]
+
+export const BEDROOMS: Array<DummyDataProps> = [
+  {
+    value: 'Studio',
+    id: 1,
+  },
+  {
+    value: '1 Bedroom',
+    id: 2,
+  },
+  {
+    value: '2 Bedroom',
+    id: 3,
+  },
+  {
+    value: '3 Bedroom',
+    id: 4,
+  },
+  {
+    value: '4+ Bedroom',
+    id: 5,
+  },
+]
+
